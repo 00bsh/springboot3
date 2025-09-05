e6cbf9af3c66ddc673afe803810ac7a4a7b47e24
Baek Soekhyeon <00bsh@tukorea.ac.kr>
2025-09-05 15:34:43 +0900

    ccccccccccccccccccc

--- springboot-app3/src/main/java/com/example/demo/UserController2.java
+++ springboot-app3/src/main/java/com/example/demo/UserController2.java
@@ -18,7 +18,7 @@ public class UserController2 {
 
     @GetMapping("/")
     public String hello() {
-        return "bbbbbbbbbbbbbbbbbbbb";
+        return "cccccccccccccccccccc";
     }
 
     @GetMapping("/mysql")
